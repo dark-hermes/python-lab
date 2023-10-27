2a188e8f2229a93ca3cb45aa51dd6ae786bbe7e8
Hermawan S Sarjito <hermawansent@gmail.com>
2023-10-27 16:49:00 +0700

    refactor: add docstring and doctest

--- modular_lab/helloFlask.py
+++ modular_lab/helloFlask.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     """Display Hello, Flask! on the web page
 
     Returns:
--- streamlit/math_operations.py
+++ streamlit/math_operations.py
@@ -1,16 +1,76 @@
 def add(x, y):
+    """Add two numbers.
+
+    Args:
+        x (int): First number.
+        y (int): Second number.
+
+    Returns:
+        int: Sum of x and y.
+        
+    >>> add(1, 2)
+    3
+    >>> add(2, 2)
+    4
+    """
     return x + y
 
 
 def subtract(x, y):
+    """Subtract two numbers.
+    
+    Args:
+        x (int): First number.
+        y (int): Second number.
+        
+    Returns:
+        int: Difference of x and y.
+        
+    >>> subtract(2, 1)
+    1
+    >>> subtract(2, 2)
+    0
+    """
     return x - y
 
 
 def multiply(x, y):
+    """
+    Multiply two numbers.
+    
+    Args:
+        x (int): First number.
+        y (int): Second number.
+        
+    Returns:
+        int: Product of x and y.
+        
+    >>> multiply(2, 2)
+    4
+    >>> multiply(2, 0)
+    0
+    """
     return x * y
 
 
 def divide(x, y):
+    """
+    Divide two numbers.
+    
+    Args:
+        x (int): First number.
+        y (int): Second number.
+        
+    Returns:
+        float: Quotient of x and y.
+        
+    >>> divide(2, 2)
+    1.0
+    >>> divide(2, 0)
+    Traceback (most recent call last):
+        ...
+    ZeroDivisionError: Division by zero is not allowed.
+    """
     if y == 0:
         raise ZeroDivisionError("Division by zero is not allowed.")
     return x / y
