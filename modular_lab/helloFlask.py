d2e284ca570c471f15d3fb1a629e7a5bb97505e3
Hermawan S Sarjito <hermawansent@gmail.com>
2023-10-27 16:34:36 +0700

    refactor: add docstring and doctest

--- modular_lab/helloFlask.py
+++ modular_lab/helloFlask.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Display Hello, Flask! on the web page
+
+    Returns:
+        str: Hello, Flask!
+    """
     return "<p>Hello, Flask!</p>"
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
